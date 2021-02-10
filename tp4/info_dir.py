87044297d763c0f0b56e5c1f74b0ecb6f7b5a019
Bwendel26 <bwendel26999@gmail.com>
2021-02-10 19:45:38 -0300

    ...

--- tp4/info_dir.py
+++ tp4/info_dir.py
@@ -9,11 +9,17 @@ import os
 import os.path
 import psutil
 
+
 def dir_info(dir_path, file_path):
+
     path = dir_path
     os.chdir(path)
     current_path = os.getcwd()
     file_size = os.path.getsize(file_path)
 
+    final = "current path:", str(current_path), "\nfile name:", \
+            "Name  -- size:", file_size,  "bytes."
+
+    return final
 
 print(dir_info("./", "info_dir.py"))
